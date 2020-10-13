07e09af302849bd19304f4e0090582a9ca4f185c
Ab Qyum <qyumjahid@gmail.com>
2020-10-13 12:11:50 +0600

    Create read.py

---
+++ read.py
@@ -0,0 +1,21 @@
+#...............................reading data..............................
+
+con=sqlite3.connect('G:/Amazon_fine_food_review_dataset/database.sqlite')
+filtered_data=pd.read_sql_query("""
+                                SELECT * FROM Reviews
+                                WHERE Score !=3 LIMIT 5000
+                                """,con)
+#print(filtered_data.head())
+def partition(x):
+    if x<3:
+        return 0
+    else:
+        return 1
+actualScore=filtered_data['Score']
+#print(actualScore)
+pos_neg=actualScore.map(partition)
+#print(pos_neg)
+filtered_data['Score']=pos_neg
+
+filtered_data.shape
+filtered_data.head()
